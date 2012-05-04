ed7bbbf5da9c5f0a4dc67cd967cf50b4d7d468c7
Martin Czygan <martin.czygan@gmail.com>
2012-05-04 22:44:28 +0200

    frontpage design

--- kollektions/views.py
+++ kollektions/views.py
@@ -2,8 +2,9 @@
 # coding: utf-8
 
 from kollektions import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
