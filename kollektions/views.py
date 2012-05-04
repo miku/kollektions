1388a4a704d3ed333f14a208ab6ea654e195ac2e
Martin Czygan <martin.czygan@gmail.com>
2012-05-04 23:29:55 +0200

    login stub

--- kollektions/views.py
+++ kollektions/views.py
@@ -8,3 +8,8 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	return render_template('login.html')
+
