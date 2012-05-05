db5814d9cbe76a69017b506e1b4e6477abfa8264
Martin Czygan <martin.czygan@gmail.com>
2012-05-05 16:46:38 +0200

    cleanup runserver.py

--- runserver.py
+++ runserver.py
@@ -2,24 +2,35 @@
 # coding: utf-8
 
 import argparse
+import pyes
+import sys
 
 if __name__ == '__main__':
-	
-	parser = argparse.ArgumentParser(description='Kollektions')
-	parser.add_argument('-s', '--server', metavar='SERVER', type=str,
-		default='development', help='server setup, one of [development, gevent]')
+    
+    parser = argparse.ArgumentParser(description='Kollektions')
+    parser.add_argument('-s', '--server', metavar='SERVER', type=str,
+        default='development', help='server setup, one of [development, gevent]')
+    parser.add_argument('--ping', action='store_true', help='ping couchdb')
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	if args.server == 'development':
-		from kollektions import app
-		app.config['DEBUG'] = True
-		app.config['SECRET_KEY'] = '0913547b7f5db1c55094f5238d666c4d'
-		app.run(host='0.0.0.0', debug=True)
+    # count the #docs in couchdb
+    if args.ping:
+        from kollektions.store import get_store, DB_URL, DB_NAME
+        __store = get_store()
+        print('%s docs in store (%s, %s)' % (len(__store), DB_URL, DB_NAME))
+        sys.exit(0)
 
-	elif args.server == 'gevent':
-		from gevent.wsgi import WSGIServer
-		from kollektions import app
+    # start server (default: development, production: gevent)
+    if args.server == 'development':
+        from kollektions import app
+        app.config['DEBUG'] = True
+        app.config['SECRET_KEY'] = '0913547b7f5db1c55094f5238d666c4d'
+        app.run(host='0.0.0.0', debug=True)
 
-		http_server = WSGIServer(('', 5000), app)
-		http_server.serve_forever()
+    elif args.server == 'gevent':
+        from gevent.wsgi import WSGIServer
+        from kollektions import app
+
+        http_server = WSGIServer(('', 5000), app)
+        http_server.serve_forever()
