b42797929f03752734df59074e777f928d305db4
Martin Czygan <martin.czygan@gmail.com>
2012-05-06 21:52:27 +0200

    more api stuff; following users; newsfeed

--- kollektions/__init__.py
+++ kollektions/__init__.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
-# database is used just for users
+# database is used just for users and social streams
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
@@ -27,6 +27,33 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    event = db.Column(db.Unicode(1024))
+    time = db.Column(db.Integer) # unix timestamp
+    data = db.Column(db.BLOB())
+   
+    def __init__(self, user_id=None, event=None, time=None, data=None):
+        self.user_id = user_id
+        self.event = event
+        self.time = time
+        self.data = data
+
+    def __repr__(self):
+        return '<Event %r>' % self.id
+
+class Following(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    following_user_id = db.Column(db.Integer, primary_key=True)
+
+    def __init__(self, user_id=None, following_user_id=None):
+        self.user_id = user_id
+        self.following_user_id = following_user_id
+
+    def __repr__(self):
+        return '<Following %r -> %r>' % (self.user_id, self.following_user_id)
 
 import kollektions.views
 
--- kollektions/views.py
+++ kollektions/views.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from kollektions import app, db, User
+from kollektions import app, db, User, Event, Following
 from kollektions.forms import LoginForm, SignupForm
+from kollektions.store import get_store
+from kollektions.utils import pretty_date
 from flask import render_template, flash, redirect, url_for, session, abort, request, jsonify
 from functools import wraps
-from sqlalchemy import or_
+from sqlalchemy import or_, and_
+from sqlalchemy.exc import IntegrityError
 
+import couchdb
+import time
+import datetime
 import urllib2
-import simplejson
+try:
+    import simplejson as json
+except ImportError:
+    import json
+import pyelasticsearch
 
 def get_book(id, bibkey = "ISBN"):
     '''Get a Book by its ID: ISBN (default), LCCN, OCLC
@@ -18,7 +28,7 @@ def get_book(id, bibkey = "ISBN"):
         A pyobj.
     '''
     url = urllib2.urlopen("http://openlibrary.org/api/books?bibkeys=%s:%s&jscmd=data&format=json" % (bibkey, id))
-    data = simplejson.load(url)['%s:%s' % (bibkey, id)]
+    data = json.load(url)['%s:%s' % (bibkey, id)]
     return data
 
 def login_required(fn):
@@ -80,14 +90,162 @@ def signup():
     return render_template('signup.html', form=form)
 
 @app.route('/users/<int:id>/')
-@login_required
 def home(id):
     user = User.query.get(id)
     if user == None:
         return abort(404)
-    if not session['user'].id == user.id:
-        return abort(403)
-    return render_template('home.html', user=user)
+    # if not session['user'].id == user.id:
+    #     return abort(403)
+
+    # __events = Event.query.filter(Event.user_id==user.id).order_by('-time')
+    followed = Following.query.filter(Following.user_id==user.id).all()
+    event_user_ids = [ f.following_user_id for f in followed ] + [user.id]
+    __events = Event.query.filter(Event.user_id.in_(event_user_ids)).order_by('-time')
+
+    events = []
+
+    event_map = {
+        'book added' : 'added',
+        'book deleted' : 'removed'
+    }
+
+    for event in __events:
+        event_data = json.loads(event.data)
+        try:
+            e = { 
+                'user_id' : event.user_id,
+                'username' : User.query.get(event.user_id).username,
+                'title' : event_data['metadata']['title'],
+                'doc_id' : event_data['doc_id'],
+                # 'time' : datetime.datetime.fromtimestamp(event.time).strftime('%d.%m.%Y %H:%M'),
+                'time' : pretty_date(datetime.datetime.fromtimestamp(event.time)),
+                'action' : event_map[event.event],
+            }
+            events.append(e)
+        except Exception as exc:
+            # print(exc)
+            pass # be graceful
+
+    # following
+    if 'user' in session:
+        following = bool(Following.query.filter(and_(
+            Following.user_id==session['user'].id, 
+            Following.following_user_id==user.id)).first())
+
+    return render_template('home.html', user=user, events=events, following=following)
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+    try:
+        isbn = request.form['isbn']
+    except KeyError:
+        abort(404)
+
+    user = session['user']
+    if user == None:
+        abort(404)
+
+    try:
+        metadata = get_book(isbn)
+    except KeyError:
+        flash('ISBN not found')
+        return redirect(url_for('home', id=user.id))
+
+    # see if user already got this books stored
+    conn = pyelasticsearch.ElasticSearch('http://localhost:9200/')
+    results = conn.search("%s AND user_id:%s" % (isbn, user.id))
+    if len(results['hits']['hits']) > 0:
+        metadata = results['hits']['hits'][0]['_source']['metadata']
+        doc_id = results['hits']['hits'][0]['_source']['_id']
+        return jsonify(key=apikey, status='200', isbn=isbn, message='OK', metadata=metadata, doc_id=doc_id)
+
+    # if not found, store it in CouchDB
+    store = get_store()
+
+    doc = {
+        "user_id" : user.id,
+        "metadata" : metadata
+    }
+    doc_id, doc_rev = store.save(doc)
+
+    # add to social stream
+    event = Event(user_id=user.id, event='book added', time=time.time(),
+        data=json.dumps({
+            "verbose" : "%s added %s to their collection" % (user.username, metadata['title']),
+            "metadata" : metadata,
+            "doc_id" : doc_id,
+        }))
+    db.session.add(event)
+    db.session.commit()
+
+    flash('Successfully added book')
+    return redirect(url_for('home', id=user.id))
+
+@app.route('/follow/<id>')
+@login_required
+def follow(id):
+    user = session['user']
+    if user == None:
+        abort(404)
+
+    followed = User.query.get(id)
+    if followed == None:
+        abort(404)
+
+    if followed.id == user.id:
+        # don't follow self
+        return redirect(url_for('home', id=followed.id))
+
+    try:
+        following = Following(user_id=user.id, following_user_id=followed.id)
+        db.session.add(following)
+        db.session.commit()
+    except IntegrityError as ie:
+        # already followed
+        db.session.rollback()
+
+    return redirect(url_for('home', id=followed.id))    
+
+@app.route('/unfollow/<id>')
+@login_required
+def unfollow(id):
+    user = session['user']
+    if user == None:
+        abort(404)
+
+    followed = User.query.get(id)
+    if followed == None:
+        abort(404)
+
+    if followed.id == user.id:
+        # don't follow self
+        return redirect(url_for('home', id=followed.id))
+
+    try:
+        following = Following.query.filter(and_(
+            Following.user_id==session['user'].id, 
+            Following.following_user_id==followed.id)).first()
+        if not following == None:
+            db.session.delete(following)
+            db.session.commit()
+    except Exception as exc:
+        db.session.rollback()
+
+    return redirect(url_for('home', id=followed.id))    
+
+
+@app.route('/item/<id>')
+def item(id):
+    store = get_store()
+    try:
+        doc = store[id]
+    except couchdb.http.ResourceNotFound, not_found:
+        return abort(404)
+
+    user = User.query.get(doc['user_id'])
+    return render_template('item.html', doc=doc, user=user)
+
 
 @app.route('/api/v1/books', methods=['PUT'])
 def api_books_put():
@@ -104,7 +262,7 @@ def api_books_put():
 
     user = User.query.filter(User.apikey==apikey).first()
     if user == None:
-        response = jsonify(apikey=apikey, isbn=isbn, status='400', message='user not found')
+        response = jsonify(apikey=apikey, status='400', message='user not found')
         response.code = 400
         return response
 
@@ -115,6 +273,105 @@ def api_books_put():
         response.code = 404
         return response
 
+    # see if user already got this books stored
+    conn = pyelasticsearch.ElasticSearch('http://localhost:9200/')
+    results = conn.search("%s AND user_id:%s" % (isbn, user.id))
+    if len(results['hits']['hits']) > 0:
+        metadata = results['hits']['hits'][0]['_source']['metadata']
+        doc_id = results['hits']['hits'][0]['_source']['_id']
+        return jsonify(key=apikey, status='200', isbn=isbn, message='OK', metadata=metadata, doc_id=doc_id)
+
+    # if not found, store it in CouchDB
+    store = get_store()
+
+    doc = {
+        "user_id" : user.id,
+        "metadata" : metadata
+    }
+    doc_id, doc_rev = store.save(doc)
+
+    # add to social stream
+    event = Event(user_id=user.id, event='book added', time=time.time(),
+        data=json.dumps({
+            "verbose" : "%s added %s to their collection" % (user.username, metadata['title']),
+            "metadata" : metadata,
+            "doc_id" : doc_id,
+        }))
+    db.session.add(event)
+    db.session.commit()
+
     # get book info by isbn ...
     # return data gathered and stored
-    return jsonify(key=apikey, status='200', isbn=isbn, message='OK', metadata=metadata)
+    return jsonify(key=apikey, status='200', isbn=isbn, message='OK', metadata=metadata, doc_id=doc_id)
+
+
+@app.route('/api/v1/books', methods=['DELETE'])
+def api_books_delete():
+    """ 
+    delete book by internal id (couchdb doc_id)
+    """
+    try:
+        apikey = request.args.get('key', None)
+        doc_id = request.args.get('id', None) # doc id
+    except KeyError:
+        response = jsonify(status='400', message='key and id parameters are required')
+        response.code = 400
+        return response
+
+    user = User.query.filter(User.apikey==apikey).first()
+    if user == None:
+        response = jsonify(apikey=apikey, status='400', message='user not found')
+        response.code = 400
+        return response
+
+    # remove book from couchdb
+    try:
+        store = get_store()
+        doc = store[doc_id]
+        metadata = doc['metadata']
+        store.delete(doc)
+    except couchdb.http.ResourceNotFound, not_found:
+        return jsonify(key=apikey, status='200', message='OK')
+
+    # add to social stream
+    event = Event(user_id=user.id, event='book deleted', time=time.time(),
+        data=json.dumps({"verbose" : "%s removed %s from their collection" % (user.username, metadata['title']) }))
+    db.session.add(event)
+    db.session.commit()
+
+    # get book info by isbn ...
+    # return data gathered and stored
+    return jsonify(key=apikey, status='200', message='OK')
+
+@app.route('/api/v1/books', methods=['GET'])
+def api_books_get():
+    try:
+        apikey = request.args.get('key', None)
+    except KeyError:
+        response = jsonify(status='400', message='key parameter is required')
+        response.code = 400
+        return response
+
+    user = User.query.filter(User.apikey==apikey).first()
+    if user == None:
+        response = jsonify(apikey=apikey, status='400', message='user not found')
+        response.code = 400
+        return response
+
+    conn = pyelasticsearch.ElasticSearch('http://localhost:9200/')
+    results = conn.search("user_id:%s" % user.id)
+
+    payload = []
+
+    for result in results['hits']['hits']:
+        item = {}
+        md = result['_source']['metadata']
+        item = {'title' : md['title'], 'id' : result['_source']['_id'] }
+        try:
+            item.update({'authors' : md['authors']})
+        except KeyError:
+            pass
+        payload.append(item)
+
+    return jsonify(key=apikey, status='200', message='OK', items=payload, count=len(results['hits']['hits']))
+
